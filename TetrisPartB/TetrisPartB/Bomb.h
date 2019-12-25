2ef5c2f30e89dc9556eed71f8ccb6b62c0fbbac3
inbalva <33578858+inbalva@users.noreply.github.com>
2019-12-25 19:58:54 +0200

    Add files via upload

---
+++ TetrisPartB/TetrisPartB/Bomb.h
@@ -0,0 +1,20 @@
+#ifndef __BOMB_H
+#define __BOMB_H
+#include "Block.h"
+
+const int BOMB_SIZE = 1;
+
+class Bomb : public Block
+{
+private:
+
+public:
+	Bomb();
+
+	//Moving the Bomb
+	virtual void move(Point::direction dir, char(&bucket)[10][15], bool &shouldBlockExplode);
+
+};
+
+#endif // !__BOMB_H
+
